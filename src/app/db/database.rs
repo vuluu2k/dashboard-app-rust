40f9d6bb37ef65384d22bb05d7a80ce79f6eabea
Vũ Lưu <vuluu@Vus-Mac-mini.local>
2025-06-06 05:39:13 +0700

    update code database

--- src/app/db/database.rs
+++ src/app/db/database.rs
@@ -9,20 +9,45 @@ cfg_if::cfg_if! {
     static DB: Lazy<Surreal<Client>> = Lazy::new(Surreal::init);
 
     pub async fn open_db_connection() {
-      DB.connect::<Ws>("ws://127.0.0.1:8000").await;
+      let _ = DB.connect::<Ws>("ws://127.0.0.1:8000").await;
 
-      DB.signin(Root {
+      let _ = DB.signin(Root {
         username: "root",
         password: "root",
       }).await;
 
-      DB.use_ns("surreal").use_db("person").await.unwrap();
+      let _ = DB.use_ns("surreal").use_db("person").await;
     }
 
     pub async fn get_all_persons() -> Option<Vec<Person>> {
       open_db_connection().await;
+
       let get_all_persons = DB.query("SELECT * FROM person ORDER BY joined_date DESC;").await;
-      DB.invalidate();
+      let _ = DB.invalidate().await;
+
+      match get_all_persons {
+        Ok(mut res) => {
+          let found = res.take(0);
+          match found {
+            Ok(found_persons) => Some(found_persons),
+            Err(_) => None,
+          }
+        }
+
+        Err(_) => None,
+      }
+    }
+
+    pub async fn add_person(new_person: Person) -> Option<Person> {
+      open_db_connection().await;
+
+      let result = DB.create(("person", new_person.uuid.clone())).content(new_person).await;
+      let _ = DB.invalidate().await;
+
+      match result {
+        Ok(created_person) => created_person,
+        Err(_) => None,
+      }
     }
   }
 }
