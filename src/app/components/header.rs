738b0c10ba0f4dc794e579e2c156cac2789612fe
Vũ Lưu <vuluu@Vus-Mac-mini.local>
2025-06-28 23:17:08 +0700

    update leptos

--- src/app/components/header.rs
+++ src/app/components/header.rs
@@ -1,4 +1,5 @@
-use leptos::prelude::*;
+use leptos::{ prelude::* };
+use leptos_router::hooks::use_location;
 
 const INPUT_STYLE: &str = "border-b-0 border-[#7734e7] h-8 text-white mx-4 hover:border-b-2";
 const INPUT_STYLE_SELECTED: &str =
@@ -8,6 +9,11 @@ const INPUT_STYLE_SELECTED: &str =
 #[component]
 pub fn Header() -> impl IntoView {
 	let (current_path, set_current_path) = signal(String::new());
+
+	Effect::new(move || {
+		let location = use_location();
+		set_current_path(location.pathname.get());
+	});
 
 	view! {
     <div class="header">
